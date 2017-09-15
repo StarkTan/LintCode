a3a3028fdf5d42297af7d42f168ca818e3a7e181
Stark <386707933@qq.com>
2017-09-15 20:13:04 +0800

    辅助类

---
+++ src/main/java/com/stark/entity/Interval.java
@@ -0,0 +1,13 @@
+package com.stark.entity;
+
+/**
+ * Created by Stark on 2017/9/8.
+ */
+public class Interval {
+    public int start, end;
+
+    public Interval(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+}
