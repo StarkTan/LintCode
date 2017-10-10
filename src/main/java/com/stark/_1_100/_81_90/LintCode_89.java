db4fb6f9c3bcbde5562add5bc5fa107be0b88432
Stark <386707933@qq.com>
2017-10-10 23:21:32 +0800

    10月10日

--- src/main/java/com/stark/_1_100/_81_90/LintCode_89.java
+++ src/main/java/com/stark/_1_100/_81_90/LintCode_89.java
@@ -8,6 +8,8 @@ import java.util.List;
 
 /**
  * Created by Stark on 2017/10/10.
+ * 给定n个不同的正整数，整数k（k < = n）以及一个目标数字。
+ * 在这n个数里面找出K个数，使得这K个数的和等于目标数字，求问有多少种方案？
  */
 public class LintCode_89 {
     public int kSum(int[] A, int k, int target) {
@@ -31,21 +33,21 @@ public class LintCode_89 {
         //由上式引出的优化
         int len = A.length;
         //动态规划dp(j , k)使用j 个数的和等于 k 的方案数
-        int[][] dp = new int[k+1][target+1];
+        int[][] dp = new int[k + 1][target + 1];
 
-        dp[0][0]=1;
-        for(int i=1;i<=len;++i){
-            for(int j=k;j>=1;j--)
-                for(int s=target;s>=A[i-1];--s)
+        dp[0][0] = 1;
+        for (int i = 1; i <= len; ++i) {
+            for (int j = k; j >= 1; j--)
+                for (int s = target; s >= A[i - 1]; --s)
                     //[j-1][k-arr[i]] 满足的集合
-                    dp[j][s] += dp[j-1][s-A[i-1]];
+                    dp[j][s] += dp[j - 1][s - A[i - 1]];
         }
 
         return dp[k][target];
     }
 
     private int solve(int[] a, int k, int targer, int begin) {
-        int result =0;
+        int result = 0;
         if (k == 1) {
             for (int i = begin; i <= a.length - 1; i++) {
                 if (a[i] == targer) {
