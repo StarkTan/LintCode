3d1e602de08237b30dd9d1df0a0ae2b57f035726
Stark <386707933@qq.com>
2018-12-06 09:22:42 +0800

    add LintCode_1163

---
+++ src/main/java/com/stark/_901_1000/_1101_1200/_1161_1170/LintCode_1163.java
@@ -0,0 +1,19 @@
+package com.stark._901_1000._1101_1200._1161_1170;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LintCode_1163 {
+    public int distributeCandies(int[] candies) {
+        // write your code here
+        int len = candies.length;
+        if (len <= 0 || len % 2 == 1) return 0;
+        Set<Integer> set = new HashSet<>();
+        for (int candie : candies) {
+            set.add(candie);
+        }
+        int typeNum = set.size();
+        if (len / 2 > typeNum) return typeNum;
+        else return len / 2;
+    }
+}
